4410c32d9337dc2d9c5454558eb4a41367debd7a
ibrahimafzal33 <ibrahimafzal33@gmail.com>
2025-11-03 22:43:38 +0500

    Update Teacher mesaage.js

--- src/components/custom/Teacher mesaage.js
+++ src/components/custom/Teacher mesaage.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import { View } from 'react-native';
+import { styles } from './src/styles/styles';
+import AboutMe from './src/components/custom/AboutMe';
+import TeacherMessage from './src/components/custom/TeacherMessage';
+import TaskbinList from './src/components/custom/TaskbinList';
+import SearchAndAdd from './src/components/custom/SearchAndAdd';
 
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <AboutMe name="Muhammad Ibrahim Afzal" regNo="Sp23-BSE-060" />
+      <TeacherMessage />
+      {/* Added below AboutMe */}
+      <TaskbinList />
+      <SearchAndAdd />
+    </View>
+  );
+}
